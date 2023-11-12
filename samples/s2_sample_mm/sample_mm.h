92f5e6c4a9413632d8e2c775c541db1a27a842aa
zer0.k <zer0kams@gmail.com>
2023-11-12 05:19:32 +0100

    Crashing sample

--- samples/s2_sample_mm/sample_mm.h
+++ samples/s2_sample_mm/sample_mm.h
@@ -20,7 +20,7 @@
 #include <iplayerinfo.h>
 #include <sh_vector.h>
 
-class SamplePlugin : public ISmmPlugin, public IMetamodListener
+class SamplePlugin : public ISmmPlugin
 {
 public:
 	bool Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late);
@@ -28,22 +28,6 @@ public:
 	bool Pause(char *error, size_t maxlen);
 	bool Unpause(char *error, size_t maxlen);
 	void AllPluginsLoaded();
-public: //hooks
-	void OnLevelInit( char const *pMapName,
-				 char const *pMapEntities,
-				 char const *pOldLevel,
-				 char const *pLandmarkName,
-				 bool loadGame,
-				 bool background );
-	void OnLevelShutdown();
-	void Hook_GameFrame( bool simulating, bool bFirstTick, bool bLastTick );
-	void Hook_ClientActive( CPlayerSlot slot, bool bLoadGame, const char *pszName, uint64 xuid );
-	void Hook_ClientDisconnect( CPlayerSlot slot, int reason, const char *pszName, uint64 xuid, const char *pszNetworkID );
-	void Hook_ClientPutInServer( CPlayerSlot slot, char const *pszName, int type, uint64 xuid );
-	void Hook_ClientSettingsChanged( CPlayerSlot slot );
-	void Hook_OnClientConnected( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, const char *pszAddress, bool bFakePlayer );
-	bool Hook_ClientConnect( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, bool unk1, CBufferString *pRejectReason );
-	void Hook_ClientCommand( CPlayerSlot nSlot, const CCommand &_cmd );
 public:
 	const char *GetAuthor();
 	const char *GetName();
